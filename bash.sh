2a2118f23bd9e08b46c3e34d162ddca7f055e396
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-15 09:16:21 +0200

    c to F/K

--- bash.sh
+++ bash.sh
@@ -1,5 +1,20 @@
 #! /bin/env bash
 
+# take 2 arg, $1 temp in C, $2 F/K
+
 cToF(){
-	 echo "scale=1"; $1 * 9/5 + 32" | bc;
+	 echo "scale=2; $1 * 9/5 + 32" | bc;
  }
+
+cToK(){
+	echo "scale=2; $1 + 273.15" | bc;
+}
+
+temp=$1
+unit=$2
+
+case $unit in 
+F) echo "$(cToF $1)";;
+K) echo "$(cToK $1)";;
+*) echo "Fail!"; exit 1;;
+esac
