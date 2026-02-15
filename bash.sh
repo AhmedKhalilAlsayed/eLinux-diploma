08219ce593bc93b2cafbef7096380d445b2bba8b
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-15 09:03:29 +0200

    fibo

--- bash.sh
+++ bash.sh
@@ -1,16 +1,5 @@
 #! /bin/env bash
 
-a=0
-b=1
-sum=0
-n=5
-
-for((i=0;i<n;i++)); do
-
-echo ${a}
-
-((sum=a+b))
-((a=b)) # a=$b
-((b=sum)) # b=sum XXXXXXXX wrong!
-
-done
+cToF(){
+	 echo "scale=1"; $1 * 9/5 + 32" | bc;
+ }
