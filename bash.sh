e4e7ecb6c65f5caa3cfe2855c32c504a1117d045
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-15 13:38:44 +0200

    simple gussing game

--- bash.sh
+++ bash.sh
@@ -1,6 +1,31 @@
 #! /bin/env bash
 
 # guess the number
-secret=$((RANDOM))
 
-echo $secret
+secret=$((RANDOM %20 + 1))
+count=1
+
+echo "I'm thinking of a nubmer between 1 and 20"
+echo "Can you guess it?"
+
+while true; do
+
+read -p "Your guess: " val
+
+if [[ ! "$val" =~ ^[0-9]+$ ]];then
+	echo "Usage: enter a number between 1 and 20"
+	continue ###########
+fi
+
+if [[ $secret -eq val ]];then
+	echo "Correct! You got it in ${count} attempts"
+	break #############
+	elif [[ val -lt $secret ]];then
+	echo "Too low"
+	((count++))
+	elif [[ val -gt $secret ]];then
+	echo "Too High"
+	((count++))
+fi
+
+done
