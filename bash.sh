f68edd0f5b42077890f3efd132562179c16b7fd5
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-15 12:06:56 +0200

    simple menu sys

--- bash.sh
+++ bash.sh
@@ -1,28 +1,21 @@
 #! /bin/env bash
 
-read -p "Enter your age: " age
+# simple menu sys
+echo "1. date"
+echo "2. List"
+echo "3. exit"
 
-# check the age is a number!
-# you can do it with regex
-set -x
-if [[ ! "$age" =~ ^[0-9]+$ ]]; then
-echo "Is not a positve number"
-exit 1
-fi 
+# -n 1: number of char
+read -n 1 -p "Enter: " ch
+echo
 
-# check range 6 to 80
-
-if [[ $age -lt 6 || $age -gt 80 ]];then
-echo "Must be in range 6 to 80 years!"
-exit 1
-fi
-
-# categorize
-if [[ $age -lt 7 ]];then
-echo "Child"
-elif [[ $age -lt 15 ]];then
-echo "Will be a man"
-else
-echo "مُكلف"
-fi
-set +x
+case $ch in
+1) date
+;;
+2)ls
+;;
+3)exit
+;;
+*) echo "Wring choice!"
+;;
+esac
