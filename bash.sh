ede52cfbbad44c7721b5df9e0bf70dd50bdb70c6
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-17 13:46:50 +0200

    rename extinsions

--- bash.sh
+++ bash.sh
@@ -1,26 +1,9 @@
 #! /bin/env bash
 
-# backup.sh
-
-set -e
-
-src_dir="$1"
-backup_dir="backups"
-timestamp=$(date +%Y%m%d_%H%M%S)
-backup_file="backup_${src_dir}_${timestamp}.tar.gz"
-
-if [[ ! -d "$src_dir" ]]; then
-echo "Error: ${src_dir} dir not exists!"
-exit 1
-fi
-
-# make sure the backup dir
-new ${backup_dir}
-
-# show loading msg
-echo "backup ..."
-
-tar -czf "${backup_dir}/${backup_file}" "${src_dir}"
-
-
-set +e
+for f in *.txt; do
+    
+    [[ -f "$f" ]] || continue
+    
+    mv "$f" "${f%.txt}"
+    # echo "${f%.txt}"
+done
