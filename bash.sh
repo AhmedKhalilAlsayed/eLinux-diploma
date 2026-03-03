37796f1ff31a80901c3484b4fe8c664b090080d0
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-03-03 08:37:20 +0200

    replacement, case sense

--- bash.sh
+++ bash.sh
@@ -4,6 +4,14 @@ set +x
 
 str="Hello Hello World!"
 
-echo "${str}"
+echo "0 ${str}"
+echo "0 ${str^^}"
+s="${str^^}"
+echo "0 ${s,}"
+echo "1 ${str//Hello/Hi}"
+echo "2 ${str//Hel*o/Hi }"
+echo "3 ${str/%World/Hi}"
+echo "4 ${str/%World!/Hi}"
+echo "5 ${str/#Hello/Hi}"
 
 set +x
