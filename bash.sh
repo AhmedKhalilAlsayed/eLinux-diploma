58522ca8f49976f65e272515880764dc4d21ad22
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-03-03 08:25:54 +0200

    str::

--- bash.sh
+++ bash.sh
@@ -1,27 +1,12 @@
 #! /bin/env bash
 
-# parameter expansion for string manipulation
-
 set +x
 
-process_path(){
-    local path=$1
-    [[ -z "$path" ]] && exit 1
-    
-    local filename="${path##*/}" # longest prefix
-    local dirname="${path%/*}" # shortest suffix
-    local basename="${filename%%.*}" # longest suffix
-    # local basename="${filename%.*}" # shortest suffix
-    local exe="${filename#*.}" # shortest prefix
-    
-    echo "$path"
-    echo "$dirname"
-	echo "$filename"
-    echo "$basename"
-    echo "$exe"
-}
+str="Ahmed"
 
-process_path "$1"
-
+echo "${str:1}"
+echo "${str: -2}" # space
+echo "${str:2:6}"
+echo "${str::7}"
 
 set +x
