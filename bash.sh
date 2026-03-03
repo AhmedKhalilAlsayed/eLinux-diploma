9193cd223dccf5c06a4d11f1b13bc839afc6cc49
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-03-03 13:04:06 +0200

    log monitor by one keyword

--- bash.sh
+++ bash.sh
@@ -1,6 +1,20 @@
 #! /bin/env bash
 
+# log file monitor
+
 set +x
+
+log_file="${1:?"Where the log file!"}"
+keywords="Error"
+
+[[ ! -f "$log_file" ]] && echo "Wrong file!" && exit 1;
+
+tail -Fn0 "$log_file" | while read -r line;do
+    if echo "$line" | grep -i "$keywords" &> /dev/null;then
+        echo "$line"
+    fi
+    
+done
 
 
 set +x
