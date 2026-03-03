ab442f01b3f07b8ca441b84ac26aa84c97062a14
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-03-03 08:10:19 +0200

    parameter expansion for string manipulation

--- bash.sh
+++ bash.sh
@@ -1,62 +1,27 @@
 #! /bin/env bash
 
+# parameter expansion for string manipulation
+
 set +x
 
-
-# validate args len
-
-if [[ $# -lt 2 ]];then
-    echo "Usage: ./script <src_files*> <des_dir>"
-    exit 1
-fi
-
-# validate are there src_files and des_d
-
-are_found="TRUE"
-
-for fd in "$@";do
-    if [[ ! -e "$fd" ]];then
-        echo "$fd: not found!"
-        are_found="FALSE"
-    fi
-done
-
-if [[ "$are_found" == "FALSE" ]];then
-    exit 1
-fi
-
-# validate that the last arg (des) is a dir
-
-args=()
-arg_len=$#
-
-
-# add all args files srcs + des
-for fd in "$@";do
-    args+=("$fd")
-done
-
-des_dir="${args[$((arg_len-1))]}"
-
-if [[ ! -d "$des_dir" ]];then
-    echo "Should the last arg is a dir!"
-    exit 1
-fi
-
-# here: validation is done
-# should all files are exist
-# should the last arg is the destenation dir
-
-for (( i=0;i<"$arg_len"-1;i++ ));do
+process_path(){
+    local path=$1
+    [[ -z "$path" ]] && exit 1
     
-    src_file="${args[$i]}"
+    local filename="${path##*/}" # longest prefix
+    local dirname="${path%/*}" # shortest suffix
+    local basename="${filename%%.*}" # longest suffix
+    # local basename="${filename%.*}" # shortest suffix
+    local exe="${filename#*.}" # shortest prefix
     
-    if ! mv  "$src_file" "$des_dir"; then
-        
-        echo "${src_file}: faild!"
-    else
-        echo "${src_file}: done!"
-    fi
-done
+    echo "$path"
+    echo "$dirname"
+	echo "$filename"
+    echo "$basename"
+    echo "$exe"
+}
+
+process_path "$1"
+
 
 set +x
