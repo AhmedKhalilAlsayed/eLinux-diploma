ef540e0a41b6cbb8ee565c7b8eca8c5181838cc9
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-03-03 13:15:17 +0200

    log monitor by many keywords, continue tracking

--- bash.sh
+++ bash.sh
@@ -5,14 +5,22 @@
 set +x
 
 log_file="${1:?"Where the log file!"}"
-keywords="Error"
+
+keywords=("Error" "log" "debbug" "fix")
 
 [[ ! -f "$log_file" ]] && echo "Wrong file!" && exit 1;
 
 tail -Fn0 "$log_file" | while read -r line;do
-    if echo "$line" | grep -i "$keywords" &> /dev/null;then
-        echo "$line"
-    fi
+    for kw in "${keywords[@]}";do
+        if echo "$line" | grep -iw "$kw" &> /dev/null;then
+            echo "$line"
+        fi
+        # can make a switch case
+        # and make a special action like alarms, noti ...
+        
+    done
+    
+    
     
 done
 
