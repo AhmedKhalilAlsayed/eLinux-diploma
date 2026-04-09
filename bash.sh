92d96df0ca6f9e8e7dd8be608a484161e6675d08
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-04-09 13:36:01 +0200

    heredoc style

--- bash.sh
+++ bash.sh
@@ -1,27 +1,19 @@
 #! /bin/env bash
 
-#! passwd generator
+#!
 
 set -e
 
 
 #! private
 #! public
-generate_passwd(){
-    local len="${1:?"Enter the length!"}"
-    
-    # if [[ len -lt 8 ]];then
-    #     echo "The len is less than 8!, please make it longer"
-    #     echo
-    # fi
-    tr -dc "A-Za-z0-9!@#$%^&*()_" < /dev/random | head -c "$len"
-    echo
-}
-
 #! main
 
-PASS=$(generate_passwd "$1")
-echo "Generated passwd: $PASS"
+name="Ahmed Khalil"
 
+cat << EOF
+hello I'm $name
+And this is a heredoc style
+EOF
 
 set +e
